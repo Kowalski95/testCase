8867a2d7ae4aa20dee05eae62fd03e72e8ef1b46
Kowalski95 <113006407+Kowalski95@users.noreply.github.com>
2022-09-07 19:32:54 +0700

    per

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ bot = telebot.TeleBot(key.readline())
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.send_message(1363480695, "Привет, я супер бот.\n Для начала новой игры введите команду: New \n Для получения информации о персонаже напишите: info ")
+	bot.send_message(message.chat.id, "Привет, я супер бот.\n Для начала новой игры введите команду: New \n Для получения информации о персонаже напишите: info ")
 
 
 @bot.message_handler(content_types=['text'])
 def message_input_step(message):
     print(message.text)
-    bot.send_message(1363480695, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 bot.polling()
