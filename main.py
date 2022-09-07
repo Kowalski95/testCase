9cb30d442af3332ee77f64b3cd1ab80281eefe9d
Kowalski95 <113006407+Kowalski95@users.noreply.github.com>
2022-09-07 19:30:22 +0700

    per

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import telebot
 
 key = open('key.txt', 'r')
 bot = telebot.TeleBot(key.readline())
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(1363480695, "Привет, я супер бот.\n Для начала новой игры введите команду: New \n Для получения информации о персонаже напишите: info ")
+
+
+@bot.message_handler(content_types=['text'])
+def message_input_step(message):
+    print(message.text)
+    bot.send_message(1363480695, message.text)
+
+bot.polling()
