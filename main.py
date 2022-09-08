172727ada40bd9160a0cc929590420f655b5ce7d
Kowalski95 <113006407+Kowalski95@users.noreply.github.com>
2022-09-08 23:41:04 +0700

    New playr.py get name playr

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 key = open('key.txt', 'r')              # В данной переменной храниться токен ботта
 bot = telebot.TeleBot(key.readline())
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler()
 def send_welcome(message):
 	bot.send_message(message.chat.id, "Привет, я супер бот.\n Для начала новой игры введите команду: New \n Для получения информации о персонаже напишите: info ")
 
@@ -13,6 +13,11 @@ def send_welcome(message):
 def message_input_step(message):
     bot.send_message(message.chat.id,'Добрый день ' + message.chat.first_name)
 
-
+# @bot.message_handler()
+# def test(message):
+#     bot.send_message(message.from_user_id, 'Hi')
 
 bot.polling()
+
+
+# https://docs-python.ru/packages/biblioteka-python-telegram-bot-python/filtry-soobschenij/
