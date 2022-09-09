fe35f94d4c158805602402d2cb5cf7c901755864
Kowalski95 <113006407+Kowalski95@users.noreply.github.com>
2022-09-09 19:34:43 +0700

    aiogram

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import telebot
+from aiogram import types
 
 
 key = open('key.txt', 'r')              # В данной переменной храниться токен ботта
 bot = telebot.TeleBot(key.readline())
 
-@bot.message_handler()
-def send_welcome(message):
-	bot.send_message(message.chat.id, "Привет, я супер бот.\n Для начала новой игры введите команду: New \n Для получения информации о персонаже напишите: info ")
+@bot.message_handler(content_types=types.ContentType.NEW_CHAT_MEMBERS)
+async def send_welcome(message: types.Message):
+	    await message.reply("hi")
 
+
+@bot.message_handler(content_types=["new_chat_members"]) #Зашел
+async def on_user_joined(message: types.Message):
+    await message.delete()
 
 @bot.message_handler(content_types=['text'])
 def message_input_step(message):
-    bot.send_message(message.chat.id,'Добрый день ' + message.chat.first_name)
+    bot.send_message(message.user.id,'Добрый день ')
 
 # @bot.message_handler()
 # def test(message):
